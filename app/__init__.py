7e3e7093173f4e4c04c667b2205d21a67794e158
Priyanshu Sharma <priyanshusharmalinkedin@gmail.com>
2026-04-28 10:26:10 +0530

    feat: add Resend API email handler for production error logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,33 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+import resend
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes,models, errors
+if not app.debug:
+    class ResendHandler(logging.Handler):
+        def emit(self, record):
+            resend.api_key = app.config['RESEND_API_KEY']
+            resend.Emails.send({
+                "from": app.config['MAIL_SENDER'],
+                "to": app.config['ADMINS'],
+                "subject": "Microblog Failure",
+                "text": self.format(record)
+            })
 
+    mail_handler = ResendHandler()
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+    
+
+
+from app import routes, models, errors
--- config.py
+++ config.py
@@ -5,8 +5,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         "sqlite:///" + os.path.join(basedir, 'app.db')
-
-
-
-
-
+    RESEND_API_KEY = os.environ.get('RESEND_API_KEY')
+    MAIL_SENDER = os.environ.get('MAIL_SENDER')
+    ADMINS = os.environ.get('ADMINS', '').split(',')
