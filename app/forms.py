73ca804d959a9764b4800649bd0f5a5508e5674d
Priyanshu Sharma <priyanshusharmalinkedin@gmail.com>
2026-05-01 12:39:01 +0530

    feat: add follow/unfollow user functionality

--- app/forms.py
+++ app/forms.py
@@ -56,3 +56,7 @@ class EditProfileForm(FlaskForm):
             if user is not None:
                 raise ValidationError('Please use a different username.')
 
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
